8f7c96b16d322fa2f72c6d10f5a8d482af041ffe
iansosa <ian_sosa996@hotmail.com>
2021-07-16 16:45:04 -0300

    git start

---
+++ CPP.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <cstring>
+
+int main()
+{
+	std::cout << "Hello World" << std::endl;
+
+	return 0;
+}
